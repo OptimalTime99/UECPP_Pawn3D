b2d156262f2fad4477df89bfb18ff5e99513e4e9
optimal <optimal9744@gmail.com>
2026-01-21 17:23:48 +0900

    Pawn에 애니메이션 적용

---
+++ Source/Pawn3DProject/Private/Pawn3DAnimInstance.cpp
@@ -0,0 +1,52 @@
+﻿#include "Pawn3DAnimInstance.h"
+#include "GameFramework/Pawn.h"
+
+UPawn3DAnimInstance::UPawn3DAnimInstance()
+{
+	GroundSpeed = 0.0f;
+	bIsMoving = false;
+	PreviousLocation = FVector::ZeroVector;
+}
+
+void UPawn3DAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerPawn = TryGetPawnOwner();
+	if (OwnerPawn)
+	{
+		PreviousLocation = OwnerPawn->GetActorLocation();
+	}
+}
+
+void UPawn3DAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (!OwnerPawn)
+	{
+		OwnerPawn = TryGetPawnOwner();
+		return;
+	}
+
+	FVector CurrentLocation = OwnerPawn->GetActorLocation();
+
+	FVector DeltaLocation = CurrentLocation - PreviousLocation;
+
+	DeltaLocation.Z = 0.0f;
+
+	double DistanceTraveled = DeltaLocation.Size();
+
+	if (DeltaSeconds > 0.0f)
+	{
+		GroundSpeed = DistanceTraveled / DeltaSeconds;
+	}
+	else
+	{
+		GroundSpeed = 0.0f;
+	}
+
+	bIsMoving = GroundSpeed > 3.0f;
+
+	PreviousLocation = CurrentLocation;
+}
---
+++ Source/Pawn3DProject/Public/Pawn3DAnimInstance.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Pawn3DAnimInstance.generated.h"
+
+UCLASS()
+class PAWN3DPROJECT_API UPawn3DAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPawn3DAnimInstance();
+
+protected:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	virtual void NativeInitializeAnimation() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character")
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character")
+	bool bIsMoving;
+
+	UPROPERTY()
+	class APawn* OwnerPawn;
+
+	FVector PreviousLocation;
+};
